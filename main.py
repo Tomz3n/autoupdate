70f504dcad9d7a31bd17520dd80643b4f5749261
Tomzen <brodythompson@bigpond.com.au>
2023-02-04 18:51:38 +0800

    main.py update
    initial code to detect updates

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from packaging import version
+import requests
 
+page = requests.get("https://raw.githubusercontent.com/Tomz3n/autoupdate/main/version.txt")
 with open('version.txt') as file:
-    currentVersion = file.read()
+    currentVersion = version.parse(file.read())
+    latestVersion = version.parse(page.text)
 
-    if isinstance(version.parse(currentVersion), version.Version): # version not invalid
-        ...
+    if isinstance(currentVersion, version.Version) and isinstance(latestVersion, version.Version): # version not invalid
+        if latestVersion > currentVersion: # not up to date
+            print("Script is out of date")
+            # perform script downloads and extraction here
+        else:
+            print("Script is up to date.")
     else:
         raise version.InvalidVersion()
